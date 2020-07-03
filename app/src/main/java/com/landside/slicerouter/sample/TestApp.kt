2c6ccac7a166a08ba11f530195f5b7590a4f8034
landscapeside <jiangshun@zbj.com>
2020-07-03 19:34:01 +0800

    fix bugs

--- app/src/main/java/com/landside/slicerouter/sample/TestApp.kt
+++ app/src/main/java/com/landside/slicerouter/sample/TestApp.kt
@@ -5,11 +5,13 @@ import android.content.Intent
 import com.landside.slicerouter.exceptions.RedirectException
 import com.landside.slicerouter.SliceDecorator
 import com.landside.slicerouter.SliceRouter
+import com.landside.slicerouter.ZipProvider
 
 class TestApp:Application() {
 
   override fun onCreate() {
     super.onCreate()
+    ZipProvider.zip(MainRouterSliceProvider())
     SliceRouter.addDecorator(object :SliceDecorator{
       override fun decorate(intent: Intent) {
         if (intent.component?.className == SecondActivity::class.java.name) {
